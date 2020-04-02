3517591b9c79d361ab6228bce2230cc30da906f4
bryant tunbutr <bryantt23@users.noreply.github.com>
2020-04-01 20:05:40 -0700

    Make the hungry moles clickable. If you click on one, add 1 point to your score.

--- scripts.js
+++ scripts.js
@@ -1,12 +1,29 @@
 let moles = document.querySelectorAll(".black-column");
 console.log(moles);
 
+let score = 0;
+
+for (const mole of moles) {
+  mole.addEventListener("click", function(event) {
+    //...
+    console.log(event);
+    if (event.target.src === "http://127.0.0.1:5500/images/mole-hungry.png") {
+      score++;
+      console.log("is a mole");
+      console.log(score);
+    } else {
+      console.log("not a mole");
+      console.log(score);
+    }
+  });
+}
+
 function hideMole(pos) {
   if (moles[pos].hasChildNodes()) {
     console.log(pos);
     moles[pos].firstChild.style.visibility = "hidden";
     // debugger;
-    setTimeout(() => showMole(pos), 3000);
+    // setTimeout(() => showMole(pos), 3000);
   }
 }
 
@@ -14,17 +31,18 @@ function showMole(pos) {
   if (moles[pos].hasChildNodes()) {
     console.log(pos + " show");
     moles[pos].firstChild.style.visibility = "visible";
+    setTimeout(() => hideMole(pos), 3000);
   }
 }
 
 const callfunction = () => {
   let pos = Math.floor(Math.random() * 11);
-  hideMole(pos);
+  showMole(pos);
 };
 
 let callCount = 1;
 let repeater = setInterval(function() {
-  if (callCount < 100) {
+  if (callCount < 99) {
     callfunction();
     callCount += 1;
   } else {
