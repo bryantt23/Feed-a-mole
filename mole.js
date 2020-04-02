11923d635c4b7c148ef1872658291ebb89bc5be7
bryant tunbutr <bryantt23@users.noreply.github.com>
2020-04-02 00:31:59 -0700

    Using object for king mole and mole

---
+++ mole.js
@@ -0,0 +1,8 @@
+function mole() {
+  return {
+    fed: "images/mole-fed.png",
+    hungry: "images/mole-hungry.png",
+    leaving: "images/mole-leaving.png",
+    sad: "images/mole-sad.png"
+  };
+}
--- scripts.js
+++ scripts.js
@@ -1,8 +1,23 @@
+const kingMole = {
+  fed: "images/king-mole-fed.png",
+  hungry: "images/king-mole-hungry.png",
+  leaving: "images/king-mole-leaving.png",
+  sad: "images/king-mole-sad.png"
+};
+
+const mole = {
+  fed: "images/mole-fed.png",
+  hungry: "images/mole-hungry.png",
+  leaving: "images/mole-leaving.png",
+  sad: "images/mole-sad.png"
+};
+
 let moles = document.querySelectorAll(".black-column");
 console.log(moles);
 
 let score = 0;
 const victoryNumber = 10;
+const urlHost = "http://127.0.0.1:5500/";
 
 for (const mole of moles) {
   mole.addEventListener("click", function(event) {
@@ -40,7 +55,8 @@ for (const mole of moles) {
       document.querySelector(".worm-container").style.width = `${wormSize}%`;
       console.log("wormSize", wormSize);
 
-      event.target.src = "images/king-mole-fed.png";
+      //   event.target.src = "images/king-mole-fed.png";
+      event.target.src = kingMole.fed;
     }
   });
 }
@@ -62,14 +78,17 @@ function hideMole(pos) {
   ) {
     moles[pos].innerHTML = "";
     let img = document.createElement("img");
-    img.src = "images/mole-sad.png";
+    // img.src = "images/mole-sad.png";
+    img.src = mole.sad;
     moles[pos].appendChild(img);
 
     setTimeout(() => {
-      moleLeaving(pos, "images/mole-leaving.png");
+      //   moleLeaving(pos, "images/mole-leaving.png");
+      moleLeaving(pos, mole.leaving);
     }, 1000);
   } else {
-    moleLeaving(pos, "images/mole-leaving.png");
+    //   moleLeaving(pos, "images/mole-leaving.png");
+    moleLeaving(pos, mole.leaving);
   }
 }
 
@@ -80,20 +99,24 @@ function hideKingMole(pos) {
   ) {
     moles[pos].innerHTML = "";
     let img = document.createElement("img");
-    img.src = "images/king-mole-sad.png";
+    // img.src = "images/king-mole-sad.png";
+    img.src = kingMole.sad;
     moles[pos].appendChild(img);
     setTimeout(() => {
-      moleLeaving(pos, "images/king-mole-leaving.png");
+      //   moleLeaving(pos, "images/king-mole-leaving.png");
+      moleLeaving(pos, kingMole.leaving);
     }, 1000);
   } else {
-    moleLeaving(pos, "images/king-mole-leaving.png");
+    //   moleLeaving(pos, "images/king-mole-leaving.png");
+    moleLeaving(pos, kingMole.leaving);
   }
 }
 
 function showKingMole(pos) {
   if (moles[pos].innerHTML === "") {
     let img = document.createElement("img");
-    img.src = "images/king-mole-hungry.png";
+    // img.src = "images/king-mole-hungry.png";
+    img.src = kingMole.hungry;
     moles[pos].appendChild(img);
     setTimeout(() => hideKingMole(pos), 3000);
   }
@@ -102,7 +125,8 @@ function showKingMole(pos) {
 function showMole(pos) {
   if (moles[pos].innerHTML === "") {
     let img = document.createElement("img");
-    img.src = "images/mole-hungry.png";
+    // img.src = "images/mole-hungry.png";
+    img.src = mole.hungry;
     moles[pos].appendChild(img);
     setTimeout(() => hideMole(pos), 3000);
   }
