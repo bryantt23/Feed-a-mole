3c962e3987472213c042cf043d7930949072ff00
bryant tunbutr <bryantt23@users.noreply.github.com>
2020-04-01 23:05:42 -0700

    Make one in ten of the moles that show up a royal mole. If a user clicks a royal mole, add 2 points to their score.

--- scripts.js
+++ scripts.js
@@ -23,31 +23,68 @@ for (const mole of moles) {
       }
       document.querySelector(".worm-container").style.width = `${wormSize}%`;
       console.log("wormSize", wormSize);
-    } else {
-      console.log("not a mole");
+    } else if (
+      event.target.src === "http://127.0.0.1:5500/images/king-mole-hungry.png"
+    ) {
+      score += 2;
+      console.log("is a mole");
       console.log("score", score);
+      let wormSize = parseInt(
+        document.querySelector(".worm-container").style.width
+      );
+      console.log("wormSize", wormSize);
+      if (wormSize === 5) {
+        wormSize += 15;
+      } else {
+        wormSize += 20;
+      }
+      document.querySelector(".worm-container").style.width = `${wormSize}%`;
+      console.log("wormSize", wormSize);
     }
   });
 }
 
 function hideMole(pos) {
-  if (moles[pos].hasChildNodes()) {
-    console.log(pos);
-    moles[pos].firstChild.style.visibility = "hidden";
-  }
+  moles[pos].innerHTML = "";
+
+  //   if (moles[pos].hasChildNodes()) {
+  //     console.log(pos);
+  //     moles[pos].firstChild.style.visibility = "hidden";
+  //   }
 }
 
-function showMole(pos) {
-  if (moles[pos].hasChildNodes()) {
-    console.log(pos + " show");
-    moles[pos].firstChild.style.visibility = "visible";
+function showKingMole(pos) {
+  if (moles[pos].innerHTML === "") {
+    var img = document.createElement("img");
+    img.src = "images/king-mole-hungry.png";
+    moles[pos].appendChild(img);
     setTimeout(() => hideMole(pos), 3000);
   }
 }
 
+function showMole(pos) {
+  if (moles[pos].innerHTML === "") {
+    var img = document.createElement("img");
+    img.src = "images/mole-hungry.png";
+    moles[pos].appendChild(img);
+    setTimeout(() => hideMole(pos), 3000);
+  }
+
+  //   if (moles[pos].hasChildNodes()) {
+  //     console.log(pos + " show");
+  //     moles[pos].firstChild.style.visibility = "visible";
+  //   setTimeout(() => hideMole(pos), 3000);
+  //   }
+}
+
 const callfunction = () => {
   let pos = Math.floor(Math.random() * 11);
-  showMole(pos);
+  if (pos === 5) {
+    //random number that represents 10%
+    showKingMole(pos);
+  } else {
+    showMole(pos);
+  }
 };
 
 let callCount = 1;
